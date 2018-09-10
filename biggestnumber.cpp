8591af8ad05b0e6e494654673cb9c7424258fb7d
Alexcyfy <acollanton@mail.csuchico.edu>
2018-09-10 16:18:54 -0700

    Asks for input of two whole numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,13 +4,14 @@
 int main()
 {
   //declare variables here
-
+  int number1; 
+  int number2; 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
-  std::cout<<"Please enter another whole number:\n";
+   std::cin>>number1;
+   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+   std::cin>>number2;
   //determine if first or second is bigger and store that in a third variable
 
 
